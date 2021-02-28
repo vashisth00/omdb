2af3f8d282720ce0e2096bf93a51fe61baf5fee6
Vashisth00 <vashisthbhushan@gmail.com>
2021-02-28 13:40:26 +0530

    Dark light switcher

---
+++ src/layout.js
@@ -0,0 +1,11 @@
+import React,{useContext} from 'react';
+import ThemeContext from './theme-context';
+
+
+const Layout = () =>{
+    const themes  = useContext(ThemeContext);
+    return
+     <div>Hi this is </div>;
+    }
+
+export default Layout; 
